f0064957128a90229863c3ad34e0634ec3caef11
Roman <ra.mezentsev@gmail.com>
2025-04-26 20:30:06 +0300

    solution2.1

--- collect_files.sh
+++ collect_files.sh
@@ -2,11 +2,16 @@
 
 declare -A count_files
 mkdir "$2"
-find "$1" -type f | while read -r path; do
+if [ -n "$3" ]; then
+    search_pattern_command="find \"$1\" -maxdepth $3 -type f"
+else
+    search_pattern_command="find \"$1\" -type f"
+fi
+eval $search_pattern_command | while read -r path; do
     name=$(basename "$path")
     сount=${count_files["$name"]:-0}
-    newname="$name"
 
+    newname="$name"
     while [[ -e "$2/$newname" ]]; do
         ((count++))
         newname="${name%.*}$count.${name##*.}"
