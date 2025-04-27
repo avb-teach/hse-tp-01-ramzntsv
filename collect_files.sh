9a8984722d4a4d48c2d61e76788d48d421bdf7c0
Roman <ra.mezentsev@gmail.com>
2025-04-27 16:36:17 +0300

    solution2.4

--- collect_files.sh
+++ collect_files.sh
@@ -2,20 +2,35 @@
 
 declare -A count_files
 mkdir "$2"
+copy_file(){
+    local name=$(basename "$1")
+    local count=${count_files["$name"]:-0}
 
-if [ -n "$3" ]; then
-    find "$1" -maxdepth "$3" -type f
-else
-    find "$1" -type f
-fi | while read -r path; do
-    name=$(basename "$path")
-    count=${count_files["$name"]:-0}
-
-    newname="$name"
-    while [[ -e "$2/$newname" ]]; do
+    local newname="$name"
+    while [[-e "$2/$newname"]]; do
         ((count++))
         newname="${name%.*}$count.${name##*.}"
     done
-        count_files["$name"]=$((count+1))
-        cp "$path" "$2/$newname"
-done
+
+    cp "$1" "$2/$newname"
+    count_files["$name"]=$((count+1))
+}
+
+if [-n "$3"]; then
+    find "$1" -type f | while read -r path; do
+        depth=$(echo "$(dirname "$path")" | tr -cd '/' | wc -c)
+        if [["$depth" -le "$3"]]; then
+            target_dir="$2/$(dirname "$path" | cut -d/ -f2-)"
+            mkdir -p "$target_dir"
+            copy_file "$path" "$target_dir"
+        else
+            target_dir="$2/$(dirname "$path" | cut -d/ -f2- | cut -d/ -f1)"
+            mkdir -p "$target_dir"
+            copy_file "$path" "$target_dir"
+        fi
+    done
+else
+    find "$1" -type f| while read -r path; do
+        copy_file "$path" "$2"
+    done
+fi 
