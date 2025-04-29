5e911e4354192bb83abf679b7461d318077e9890
Roman <ra.mezentsev@gmail.com>
2025-04-29 13:38:19 +0300

    final_solution

--- collect_files.sh
+++ collect_files.sh
@@ -1,35 +1,48 @@
 #!/bin/bash
 
+input_dir="$1"
+output_dir="$2"
 declare -A count_files
-mkdir "$2"
-copy_file(){
+mkdir "$output_dir"
+
+copy_file_no_maxdepth(){
     local name=$(basename "$1")
     local count=${count_files["$name"]:-0}
 
     local newname="$name"
-    while [[ -e "$2/$newname" ]]; do
+    while [[ -e "$output_dir/$newname" ]]; do
         ((count++))
         newname="${name%.*}$count.${name##*.}"
     done
 
-    cp "$1" "$2/$newname"
+    cp "$1" "$output_dir/$newname"
     count_files["$name"]=$((count+1))
 }
-if [-n "$4"]; then
-    find "$1" -type f | while read -r path; do
-        depth=$(echo "$(dirname "$path")" | tr -cd '/' | wc -c)
-        if [["$depth" -le "$4"]]; then
-            target_dir="$2/$(dirname "$path" | cut -d/ -f2-)"
-            mkdir -p "$target_dir"
-            copy_file "$path" "$target_dir"
-        else
-            target_dir="$2/$(dirname "$path" | cut -d/ -f2- | cut -d/ -f1)"
-            mkdir -p "$target_dir"
-            copy_file "$path" "$target_dir"
-        fi
-    done
-else
-    find "$1" -type f| while read -r path; do
-        copy_file "$path" "$2"
-    done
-fi 
+copy_dir_max_depth(){
+    local path="$1"
+    local max_depth="$2"
+    local new_path="${path#$input_dir/}"
+    local depth=$(echo "$new_path" | grep -o "/" | wc -l)
+    local target_dir
+    if (( depth > "$max_depth" )); then
+        local cut_path=$(echo "$new_path" | cut -d'/' -f1-"$max_depth")
+        target_dir="$output_dir/$(dirname "$cut_path")"
+
+    else
+        target_dir="$output_dir/$(dirname "$new_path")"
+    fi
+
+    mkdir -p "$target_dir"
+    cp "$1" "$target_dir/"
+}
+
+find "$input_dir" -type f | while read -r path; do
+    if [ -n "$4" ]; then
+        max_depth="$4"
+        copy_dir_max_depth "$path" "$max_depth"
+
+    else
+        copy_file_no_maxdepth "$path" 
+    fi
+done
+
